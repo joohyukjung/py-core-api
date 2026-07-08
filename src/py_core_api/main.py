b960b0f51ad937d3764884c6aa7b3e96cdb839b6
joohyukjung <joohyukjung@joohyukjungui-MacBookAir.local>
2026-07-08 09:48:00 +0900

    ty test

--- src/py_core_api/main.py
+++ src/py_core_api/main.py
@@ -2,5 +2,11 @@ def main():
     print("Hello from py-core-api!")
 
 
+def add(a: int, b: int) -> int:
+    return a + b
+
+
 if __name__ == "__main__":
     main()
+    result = add(2, 5)
+    print(result)
